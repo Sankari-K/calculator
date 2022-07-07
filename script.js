891ee5a028977b15e0a3bb7a97cea33696c9375e
Sankari Karthik <sankarikarthik03@gmail.com>
2022-07-07 14:03:54 +0530

    Add function to call the other arithmetic functions

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function add(a, b) {
     return a + b;
-};
+}
 
 function subtract(a, b) {
     return a - b;
@@ -11,5 +11,24 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-    return a / b;
+    return +((a / b).toFixed(2));
+}
+
+function operate(operator, num1, num2) {
+    let res;
+    switch(operator) {
+        case '+':
+            res = add(num1, num2);
+            break;
+        case '-':
+            res = subtract(num1, num2);
+            break;
+        case '*':
+            res = multiply(num1, num2);
+            break;
+        case '/':
+            res = divide(num1, num2);
+            break;
+    }
+    return res;
 }
