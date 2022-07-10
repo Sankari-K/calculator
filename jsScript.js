13377a06f223d8ee513cdf2a7262ca0782b72952
Sankari Karthik <sankarikarthik03@gmail.com>
2022-07-10 12:19:02 +0530

    Add keyboard support and comments for JS file

--- jsScript.js
+++ jsScript.js
@@ -1,3 +1,4 @@
+// Define variables for certain elements of DOM
 let backgroundFrame = document.querySelector(".frame");
 const backgroundStyle = getComputedStyle(backgroundFrame);
 
@@ -17,11 +18,17 @@ let displayFlag = false;
 
 let clear = document.querySelector("#clear");
 
+// These are the values that user inputs
 let num1 = '';
 let num2 = '';
 let operator = ''; 
-let firstTime = true;
 
+// Flag to check if this is the first time the user types
+let firstTime = true;
+// Flag to know whether the input is from the keyboard 
+let keyInputFlag = false;
+
+// Event listeners for all 3 types of buttons
 numberButtons.forEach((button) =>
     button.addEventListener('click', getNumInput));
 
@@ -31,7 +38,9 @@ operatorButtons.forEach((button) =>
 unaryButtons.forEach((button) =>
     button.addEventListener('click', getUnaryInput));
 
+// In case a number key is pressed
 function getNumInput(e) {
+    // To prevent the calculator from losing it's shape
     if (display.innerText.length >= 17) {
         window.alert("Overflow! The calculator can't solve such large calculations :(");
         clearAllElements();
@@ -40,14 +49,24 @@ function getNumInput(e) {
         cleanUpDisplay();
         firstTime = false;
     }
+
+    // If a number is pressed right after a calculation (a = is pressed), clear data and proceed with new calculation
     if (operatorPressed == '=') {   
-        //console.log("is operator equals");
         display.innerText = '';
         num1 = '';
         operatorPressed = ''; 
     }
-    let keyPressed = e.composedPath()[0].innerText; // of type string
+    // For keyboard functionality 
+    let keyPressed;
+    if (keyInputFlag) {
+        keyPressed = e;
+        keyInputFlag = false;
+    }
+    else {
+        keyPressed = e.composedPath()[0].innerText; // of type string
+    }
     
+    // To know whether the input number belongs to num1 or num2
     if (!operator) {
         if (isSmall(num1) && isValidDecimal(keyPressed, num1)) {   
             num1 += keyPressed;
@@ -60,21 +79,25 @@ function getNumInput(e) {
             display.innerText += keyPressed;
         } 
     }
-    // console.log(keyPressed, operatorPressed);
-    // console.log("num1", num1);
-    // console.log("num2", num2);
-    // console.log("operator", operator);
 }
 
 let operatorPressed;
-function getOperatorInput(e) {
-    
+function getOperatorInput(e) {   // If it is +,  -, /, or * operators
+    // To prevent the calculator from losing it's shape
     if (display.innerText.length > 17) {
         window.alert("Overflow! The calculator can't solve such large calculations :(");
         clearAllElements();
     } 
 
-    operatorPressed = e.composedPath()[0].innerText;
+    // For keyboard functionality 
+    if (keyInputFlag) {
+        operatorPressed = e;
+        keyInputFlag = false;
+    }
+    else {
+        operatorPressed = e.composedPath()[0].innerText;
+    }
+
     if (operatorPressed != "=") {
         if (operator != '') { // If an operator was pressed already, evaluate that previous expression first
             // If num2 ain't defined, just switch the operator
@@ -95,8 +118,8 @@ function getOperatorInput(e) {
         operator = operatorPressed;
         display.innerText += operator;
     }
-    else { // If it is +,  -, /, or * operators
-        if (num1 && num2) {
+    else { 
+        if (num1 && num2) { // If both numbers are defined, proceed with evaluation
             display.innerText = operate(operator, num1, num2);
             if (display.innerText == Infinity || display.innerText == NaN) {
                 window.alert("That's a math error -_-");
@@ -105,7 +128,7 @@ function getOperatorInput(e) {
                 return;
             }
             operator = '';
-            num1 = display.innerText;
+            num1 = display.innerText; // DIsplaying the result, and assigning it to num1 for next calculation
             num2 = '';
         }
         else {
@@ -115,14 +138,21 @@ function getOperatorInput(e) {
 }
 
 function getUnaryInput(e) {
-    console.log(e);
-    console.log(num1, num2);
-    console.log("the last operator was", operator);
-    
-    let unaryOperatorPressed = e.composedPath()[0];
-    if (unaryOperatorPressed.innerText == '%') {
+    // For keyboard support
+    let unaryOperatorPressed;
+    if (keyInputFlag) {
+        unaryOperatorPressed = e;
+        keyInputFlag = false;
+    }
+    else {
+        unaryOperatorPressed = e.composedPath()[0].innerText;
+    }
+
+    if (unaryOperatorPressed == '%') {
         if (operator) {
-            display.innerText = display.innerText.replace(num2, num2 / 100);
+            // Replace last instance of num (in display text) with 0.01 times that
+            let n = display.innerText.lastIndexOf(num2);
+            display.innerText = display.innerText.slice(0, n) + display.innerHTML.slice(n).replace(num2, num2/100);
             num2 = num2/100;
         }
         else {
@@ -132,7 +162,9 @@ function getUnaryInput(e) {
     }
     else {
         if (operator) {
-            display.innerText = display.innerText.replace(num2, -1 * num2);
+            // Replace last instance of num (in display text) with -1 times that
+            let n = display.innerText.lastIndexOf(num2);
+            display.innerText = display.innerText.slice(0, n) + display.innerHTML.slice(n).replace(num2, -1 * num2);
             num2 = -1 * num2;
         }
         else {
@@ -142,6 +174,7 @@ function getUnaryInput(e) {
     }
 }
 
+// Helper functions
 function cleanUpDisplay() {
     display.innerText = '';
 }
@@ -157,13 +190,24 @@ function clearAllElements() {
     operatorPressed = ''; 
 }
 
+// Such a condition is being checked to prevent overflow of calculator
+function isSmall(n) { 
+    return n.length < 8;
+}
 
+// Should not be like 5.67.8
+function isValidDecimal(key, num) {
+    return !(key == '.' && num.includes("."));
+}
+
+
+// Functions for actual calculations
 function add(a, b) {
-    return a + b;
+    return +((a + b).toFixed(4));
 }
 
 function subtract(a, b) {
-    return a - b;
+    return +((a - b).toFixed(4));
 }
 
 function multiply(a, b) {
@@ -188,21 +232,14 @@ function operate(operator, num1, num2) {
         case 'x':
             res = multiply(num1, num2);
             break;
-        case '÷':
+        case '/':
             res = divide(num1, num2);
             break;
     }
     return res;
 }
 
-function isSmall(n) { 
-    return n.length < 8;
-}
 
-
-function isValidDecimal(key, num) {
-    return !(key == '.' && num.includes("."));
-}
 // Light and dark mode toggle
 themeToggle.addEventListener('click', function() {
     // Change color of calculator 
@@ -225,4 +262,18 @@ themeToggle.addEventListener('click', function() {
     // Change background color of page 
     html.style.backgroundColor = htmlStyle.backgroundColor == "rgba(226, 207, 201, 0.6)" ? 
     "rgba(153, 159, 165, 0.6)" : "rgba(226, 207, 201, 0.6)"; 
+})
+
+// Event listeners for keyboard support
+window.addEventListener('keydown', function(e) {
+    keyInputFlag = true;
+    if ('1234567890.'.includes(e.key)) {
+        getNumInput(e.key);
+    }
+    else if ('=+-x/'.includes(e.key)) {
+        getOperatorInput(e.key);
+    }
+    else if ('%±'.includes(e.key)) {
+        getUnaryInput(e.key);
+    }
 })
